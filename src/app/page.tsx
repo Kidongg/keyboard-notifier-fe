351629cf405b38f0bddfdb13eaf38c882ec53847
devdubby <cjsoung@naver.com>
2024-10-19 16:20:30 +0900

    feat: 공제 리스트 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import classNames from 'classnames/bind';
+
+import GBItemList from '@/app/components/GBItemList';
+
+import styles from './page.module.scss';
+
+const cx = classNames.bind(styles);
+
 export default function Home() {
-  return <main>메인 페이지</main>;
+  return (
+    <main className={cx('main')}>
+      <GBItemList />
+    </main>
+  );
 }
