c9704fcd553807164b1c16382ef7a3b4ee32a12e
Kidongg <dlrlehd7@daum.net>
2024-11-11 23:44:58 +0900

    feat: carousel 추가

--- src/app/(detail)/components/GBItemCarousel/GBItemCarousel.tsx
+++ src/app/(detail)/components/GBItemCarousel/GBItemCarousel.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import classNames from 'classnames/bind';
+import Slider from 'react-slick';
+
+import styles from './GBItemCarousel.module.scss';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const cx = classNames.bind(styles);
+
 const GBItemCarousel = () => {
-  return <section>캐러셀</section>;
+  const settings = {
+    arrows: false,
+    dots: true,
+    appendDots: (dots: React.ReactNode) => (
+      <div style={{ position: 'absolute', bottom: '20px', left: '50%', transform: 'translateX(-50%)', zIndex: 1 }}>
+        <ul style={{ margin: '0px' }}> {dots} </ul>
+      </div>
+    ),
+  };
+
+  return (
+    <section className={cx('container')}>
+      <Slider {...settings}>
+        <img className={cx('image')} src="/assets/images/gb-thumbnail.png" alt="image" width="100%" height="520px" />
+        <img className={cx('image')} src="/assets/images/gb-thumbnail.png" alt="image" width="100%" height="520px" />
+        <img className={cx('image')} src="/assets/images/gb-thumbnail.png" alt="image" width="100%" height="520px" />
+        <img className={cx('image')} src="/assets/images/gb-thumbnail.png" alt="image" width="100%" height="520px" />
+        <img className={cx('image')} src="/assets/images/gb-thumbnail.png" alt="image" width="100%" height="520px" />
+      </Slider>
+    </section>
+  );
 };
 
 export default GBItemCarousel;
--- src/app/(detail)/components/GBItemInformation/GBItemInformation.tsx
+++ src/app/(detail)/components/GBItemInformation/GBItemInformation.tsx
@@ -1,5 +1,11 @@
+import classNames from 'classnames/bind';
+
+import styles from './GBItemInformation.module.scss';
+
+const cx = classNames.bind(styles);
+
 const GBItemInformation = () => {
-  return <div>정보</div>;
+  return <div className={cx('container')}>정보</div>;
 };
 
 export default GBItemInformation;
