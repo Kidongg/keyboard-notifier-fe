8fdc38438dab4623341526eadfe95ddf31fa5a3d
Kidongg <dlrlehd7@daum.net>
2024-11-13 23:25:09 +0900

    feat: text 영역 추가

--- src/app/(detail)/components/GBItemInformation/GBItemInformation.tsx
+++ src/app/(detail)/components/GBItemInformation/GBItemInformation.tsx
@@ -1,11 +1,32 @@
 import classNames from 'classnames/bind';
 
+import Badge from '@/app/(detail)/components/Badge/Badge';
+
 import styles from './GBItemInformation.module.scss';
 
 const cx = classNames.bind(styles);
 
 const GBItemInformation = () => {
-  return <div className={cx('container')}>정보</div>;
+  return (
+    <section className={cx('container')}>
+      <div className={cx('badges')}>
+        <Badge text="진행예정" type="status" />
+        <Badge text="키보드" type="category" />
+      </div>
+      <div className={cx('texts')}>
+        <p className={cx('title')}>[GB] GMK Rubrehose Artisanchoke</p>
+        <p className={cx('price')}>
+          <span className={cx('value')}>68,000</span>
+          <span className={cx('currency')}>원</span>
+        </p>
+        <p className={cx('period')}>2024.01 - 2024.07</p>
+      </div>
+      <div className={cx('buttons')}>
+        <button>오픈 알림 신청</button>
+        <button>해당상품 보러가기</button>
+      </div>
+    </section>
+  );
 };
 
 export default GBItemInformation;
