b024a0846c802ab90ffdf0ea89c62fde8596f213
Kidongg <dlrlehd7@daum.net>
2024-11-17 17:26:30 +0900

    feat: GBItemList 추가

---
+++ src/app/(detail)/components/GBItem/GBItem.tsx
@@ -0,0 +1,28 @@
+import classNames from 'classnames/bind';
+
+import Badge from '@/app/(detail)/components/Badge/Badge';
+
+import styles from './GBItem.module.scss';
+
+const cx = classNames.bind(styles);
+
+const GBItem = () => {
+  return (
+    <li className={cx('list')} role="button">
+      <div className={cx('badge-wrap')}>
+        <Badge text="진행예정" type="status" />
+        <Badge text="키보드" type="category" />
+      </div>
+      <div className={cx('content-wrap')}>
+        <div className={cx('title')}>[GB] GMK Rubrehose Artisanchoke Idkw...</div>
+        <div className={cx('price')}>
+          <span className={cx('value')}>68,000</span>
+          <span className={cx('currency')}>원</span>
+        </div>
+        <div className={cx('date')}>2021.01 - 2024.07</div>
+      </div>
+    </li>
+  );
+};
+
+export default GBItem;
--- src/app/(detail)/components/GBItemList/GBItemList.tsx
+++ src/app/(detail)/components/GBItemList/GBItemList.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import classNames from 'classnames/bind';
+
+import GBItem from '@/app/(detail)/components/GBItem/GBItem';
+
+import styles from './GBItemList.module.scss';
+
+const cx = classNames.bind(styles);
 
 const GBItemList = () => {
-  return <div>리스트</div>;
+  return (
+    <ul className={cx('list-container')}>
+      {Array.from({ length: 6 }).map((_, index) => (
+        <GBItem key={index} />
+      ))}
+    </ul>
+  );
 };
 
 export default GBItemList;
