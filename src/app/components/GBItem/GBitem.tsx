30ecc44c4fa0e3cd849a3ae8325d08fdd70aebc1
Kidongg <dlrlehd7@daum.net>
2024-12-10 13:32:27 +0000

    feat: 메인페이지 api 연동

--- src/app/components/GBItem/GBitem.tsx
+++ src/app/components/GBItem/GBitem.tsx
@@ -1,22 +1,50 @@
 import classNames from 'classnames/bind';
 
-import Chip from '@/app/components/Chip';
+import ProductCategoryTypeChip from '@/app/(detail)/components/ProductCategoryTypeChip';
+import { formatDate } from '@/app/(shared)/utils/date';
+import { formatPrice } from '@/app/(shared)/utils/price';
 import Notification from '@/app/components/Notification';
+import ProductStatusChip from '@/app/components/ProductStatusChip';
+import { ProductCategoryType, ProductStatusType } from '@/app/types/api/product';
 
 import styles from './GBitem.module.scss';
 
 const cx = classNames.bind(styles);
 
-const GBItem = () => {
+type GBItemProps = {
+  name: string;
+  price: number;
+  unit: string;
+  startDate: string;
+  endDate: string;
+  imageUrl: string[];
+  productUrl: string;
+  status: ProductStatusType;
+  categoryType: ProductCategoryType;
+};
+
+const GBItem = ({ name, price, unit, startDate, endDate, imageUrl, productUrl, status, categoryType }: GBItemProps) => {
+  const moveToDetailPage = () => {
+    window.open(productUrl, '_blank', 'noopener noreferrer');
+  };
+
   return (
-    <li className={cx('list')} role="button">
+    <li
+      className={cx('list')}
+      role="button"
+      style={{
+        backgroundImage: `url(${imageUrl[0]})`,
+      }}
+      onClick={moveToDetailPage}
+    >
       <div className={cx('chip-wrap')}>
-        <Chip />
+        <ProductStatusChip status={status} />
+        <ProductCategoryTypeChip categoryType={categoryType} />
       </div>
       <div>
-        <h3 className={cx('title')}>[GB] GMK Rubrehose Artisanchoke Idkw...</h3>
-        <p className={cx('price')}>68,000 원</p>
-        <p className={cx('date')}>2021.01 - 2024.07</p>
+        <h3 className={cx('name')}>{name}</h3>
+        <p className={cx('price')}>{formatPrice(price, unit)}</p>
+        <p className={cx('date')}>{`${formatDate(startDate)} - ${formatDate(endDate)}`}</p>
       </div>
       <Notification />
     </li>
--- src/app/components/GBItemList/index.ts
+++ src/app/components/GBItemList/index.ts
@@ -1,1 +1,1 @@
-export { default } from './GBItemList';
+export { default } from './ProductList';
