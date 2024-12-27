298ec643721ce07aee50ebaf00f73c726738d346
Kidongg <dlrlehd7@daum.net>
2024-12-27 17:11:58 +0900

    feat: 404 에러 페이지 로직

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+
 import classNames from 'classnames/bind';
 
 import ErrorActionButton from '@/app/components/ErrorActionButton/ErrorActionButton';
@@ -9,6 +11,8 @@ import styles from './not-found.module.scss';
 const cx = classNames.bind(styles);
 
 const Notfound = () => {
+  const router = useRouter();
+
   return (
     <main className={cx('main')}>
       <div className={cx('logo')}>
@@ -20,8 +24,8 @@ const Notfound = () => {
         주소를 다시 확인해주세요.
       </div>
       <div className={cx('buttons')}>
-        <ErrorActionButton text="돌아가기" action={() => {}} />
-        <ErrorActionButton text="홈으로 이동" action={() => {}} />
+        <ErrorActionButton text="돌아가기" action={() => router.back()} />
+        <ErrorActionButton text="홈으로 이동" action={() => router.push('/')} />
       </div>
     </main>
   );
